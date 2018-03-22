4c4a4187398a721e211972462884528e4c7deb1e
hydro <ihydrodog@gmail.com>
2018-03-22 22:18:16 +0900

    save progress

--- Nekodong.py
+++ Nekodong.py
@@ -3,49 +3,140 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import math
 
 
 
-class CardPlayer( gym.Env ):
+class CardPlay( gym.Env ):
+
+    class Player():
+        def __init__(self, cardCount ):
+            self.reset()
+
+
+        def randomChoice(self):
+            return self.pop( self._deck[ random.randrange(len(self._deck))] )
+
+
+        def contains(self, x):
+            return x in self._deck
+
+
+        def reset(self):
+            self._deck = [i+1 for i in range(self.cardCount)]
+            self._dumped = []
+
+
+        def pop(self, x ):
+            self._deck.pop(x)
+            self._dumped.append( x )
+
+
+        def empty(self):
+            return len( self._deck ) == 0
+
+
+        @property
+        def dumped(self):
+            return self._dumped
+
+
 
 
     class Action( gym.Space ):
         def __init__(self, cardCount ):
             self._cardCount = cardCount
-            self._deck = [i+1 for i in range(self.cardCount)]
+            self._player = CardPlay.Player( cardCount )
 
 
         def sample(self):
-            return self._deck.pop( random.randrange( len( self._deck ) ) )
+            return self._player.randomChoice()
 
 
         def contains(self, x):
-            return x in self._deck
+            return self._player.contains( x )
+
+
+        def reset(self):
+            self._player.reset()
+
+
+        def step(self, x):
+            self._player.pop( x )
+
+
+        def isDone(self):
+            return self.empty()
+
+
+        def getDumped(self):
+            return self._player.dumped
 
 
 
     class Observation( gym.Space ):
         def __init__(self, cardCount ):
             self._cardCount = cardCount
-            self._deck = [i+1 for i in range(self.cardCount)]
+            self._max = math.pow( math.factorial( self._cardCount ), 2)
+
 
         def sample(self):
-            return self._deck.pop(random.randrange(len(self._deck)))
+            return random.randrange( self._max )
 
         def contains(self, x):
-            return x in self._deck
+            return 0 <= x < self._max
+
 
 
     def __init__(self):
-        self.action_space = CardPlayer.Action()
-        self.observation_space = CardPlayer.Observation()
+        self.cardCount = 5
+        self.action_space = CardPlay.Action( self.cardCount )
+        self.observation_space = CardPlay.Observation()
+
+        self._opponent = CardPlay.Player( self.cardCount )
+        self._reset()
+
+
+    def _getState(self, a, b):
+        for _a, _b in zip( a, b):
+
+
 
     def _step(self, action):
-        pass
+        self.action_space.step( action )
+        done = self.action_space.isDone()
+
+        opponentCard = self._opponent.randomChoice()
+
+        score = 1 if action > opponentCard else 0 if action == opponentCard else -1
+
+        mine = self.action_space.getDumped()
+        opp = self._opponent.dumped
+
+        reward = 0
+        if done:
+            wins = 0
+            draws = 0
+            for me, opponent in zip( mine, opp ):
+                if me > opponent:
+                    wins += 1
+                elif me == opponent:
+                    draws += 1
+            if wins > self.cardCount//2:
+                reward = 1
+
+
+        state = self._getState( mine, opp )
+        return state, reward, done, None
+
+
 
 
     def _reset(self):
-        pass
+        self.action_space.reset()
+        self._state = 0
+        self._opponent.reset()
+
 
 def one_hot( s, max_samples = 16):
     return np.identity( max_samples)[s:s+1]
