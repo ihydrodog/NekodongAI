ede495d6fa757b507982119e15bc294ecc9dc445
hydro <ihydrodog@gmail.com>
2018-03-26 23:24:58 +0900

    save progress

--- Nekodong.py
+++ Nekodong.py
@@ -6,7 +6,127 @@ import matplotlib.pyplot as plt
 import random
 import math
 import itertools
+from collections import deque
+from typing import List
 
+
+class DQN:
+
+    def __init__(self, session: tf.Session, input_size: int, output_size: int, name: str="main") -> None:
+        """DQN Agent can
+        1) Build network
+        2) Predict Q_value given state
+        3) Train parameters
+        Args:
+            session (tf.Session): Tensorflow session
+            input_size (int): Input dimension
+            output_size (int): Number of discrete actions
+            name (str, optional): TF Graph will be built under this name scope
+        """
+        self.session = session
+        self.input_size = input_size
+        self.output_size = output_size
+        self.net_name = name
+
+        self._build_network()
+
+    def _build_network(self, h_size=16, l_rate=0.001) -> None:
+        """DQN Network architecture (simple MLP)
+        Args:
+            h_size (int, optional): Hidden layer dimension
+            l_rate (float, optional): Learning rate
+        """
+        with tf.variable_scope(self.net_name):
+            self._X = tf.placeholder(tf.float32, [None, self.input_size], name="input_x")
+            net = self._X
+
+            net = tf.layers.dense(net, h_size, activation=tf.nn.relu)
+            net = tf.layers.dense(net, self.output_size)
+            self._Qpred = net
+
+            self._Y = tf.placeholder(tf.float32, shape=[None, self.output_size])
+            self._loss = tf.losses.mean_squared_error(self._Y, self._Qpred)
+
+            optimizer = tf.train.AdamOptimizer(learning_rate=l_rate)
+            self._train = optimizer.minimize(self._loss)
+
+    def predict(self, state: np.ndarray) -> np.ndarray:
+        """Returns Q(s, a)
+        Args:
+            state (np.ndarray): State array, shape (n, input_dim)
+        Returns:
+            np.ndarray: Q value array, shape (n, output_dim)
+        """
+        x = np.reshape(state, [-1, self.input_size])
+        return self.session.run(self._Qpred, feed_dict={self._X: x})
+
+    def update(self, x_stack: np.ndarray, y_stack: np.ndarray) -> list:
+        """Performs updates on given X and y and returns a result
+        Args:
+            x_stack (np.ndarray): State array, shape (n, input_dim)
+            y_stack (np.ndarray): Target Q array, shape (n, output_dim)
+        Returns:
+            list: First element is loss, second element is a result from train step
+        """
+        feed = {
+            self._X: x_stack,
+            self._Y: y_stack
+        }
+        return self.session.run([self._loss, self._train], feed)
+
+
+
+def replay_train(mainDQN: DQN, targetDQN: DQN, train_batch: list) -> float:
+    """Trains `mainDQN` with target Q values given by `targetDQN`
+    Args:
+        mainDQN (dqn.DQN): Main DQN that will be trained
+        targetDQN (dqn.DQN): Target DQN that will predict Q_target
+        train_batch (list): Minibatch of replay memory
+            Each element is (s, a, r, s', done)
+            [(state, action, reward, next_state, done), ...]
+    Returns:
+        float: After updating `mainDQN`, it returns a `loss`
+    """
+    DISCOUNT_RATE = 0.99
+
+
+    states = np.vstack([x[0] for x in train_batch])
+    actions = np.array([x[1] for x in train_batch])
+    rewards = np.array([x[2] for x in train_batch])
+    next_states = np.vstack([x[3] for x in train_batch])
+    done = np.array([x[4] for x in train_batch])
+
+    X = states
+
+    Q_target = rewards + DISCOUNT_RATE * np.max(targetDQN.predict(next_states), axis=1) * ~done
+
+    y = mainDQN.predict(states)
+    y[np.arange(len(X)), actions] = Q_target
+
+    # Train our network using target and predicted Q values on each episode
+    return mainDQN.update(X, y)
+
+
+def get_copy_var_ops(*, dest_scope_name: str, src_scope_name: str) -> List[tf.Operation]:
+    """Creates TF operations that copy weights from `src_scope` to `dest_scope`
+    Args:
+        dest_scope_name (str): Destination weights (copy to)
+        src_scope_name (str): Source weight (copy from)
+    Returns:
+        List[tf.Operation]: Update operations are created and returned
+    """
+    # Copy variables src_scope to dest_scope
+    op_holder = []
+
+    src_vars = tf.get_collection(
+        tf.GraphKeys.TRAINABLE_VARIABLES, scope=src_scope_name)
+    dest_vars = tf.get_collection(
+        tf.GraphKeys.TRAINABLE_VARIABLES, scope=dest_scope_name)
+
+    for src_var, dest_var in zip(src_vars, dest_vars):
+        op_holder.append(dest_var.assign(src_var.value()))
+
+    return op_holder
 
 
 
@@ -23,7 +143,7 @@ class CardPlay( gym.Env ):
 
 
         def contains(self, x):
-            print( self._deck, x)
+            # print( self._deck, x)
             return x in self._deck
 
 
@@ -123,6 +243,11 @@ class CardPlay( gym.Env ):
         self._reset()
 
 
+    @property
+    def Opponent(self):
+        return self._opponent
+
+
     def _getState(self, a, b):
         return self._stateMap[ (tuple(a), tuple(b)) ]
 
@@ -137,7 +262,12 @@ class CardPlay( gym.Env ):
             opponentCard = self._opponent.randomChoice()
             self._opponent.pop( opponentCard )
 
-            score = 1 if action > opponentCard else 0 if action == opponentCard else -1
+            if action == 0 and opponentCard == self.cardCount-1:
+                score = 1
+            elif action == self.cardCount-1 and opponentCard == 0:
+                score = -1
+            else:
+                score = 1 if action > opponentCard else 0 if action == opponentCard else -1
 
             mine = self.action_space.getDumped()
             opp = self._opponent.dumped
@@ -186,55 +316,93 @@ def testRun():
     input_size = env.observation_space.n
     output_size = env.action_space.n
 
-    X = tf.placeholder( shape=[1, input_size], dtype=tf.float32 )
-    W = tf.Variable( tf.random_uniform( [input_size, output_size], 0, 0.01 ) )
-    Qpred = tf.matmul( X, W )
-    Y = tf.placeholder( shape=[1, output_size], dtype=tf.float32 )
-    loss = tf.reduce_sum( tf.square( Y-Qpred))
-
-    train = tf.train.GradientDescentOptimizer( learning_rate=0.1).minimize( loss )
-    discount = 0.99
-    num_episodes = 10000
+    # X = tf.placeholder( shape=[1, input_size], dtype=tf.float32 )
+    # W = tf.Variable( tf.random_uniform( [input_size, output_size], 0, 0.01 ) )
+    # Qpred = tf.matmul( X, W )
+    # Y = tf.placeholder( shape=[1, output_size], dtype=tf.float32 )
+    # loss = tf.reduce_sum( tf.square( Y-Qpred))
+    #
+    # train = tf.train.GradientDescentOptimizer( learning_rate=0.1).minimize( loss )
+    # discount = 0.99
+    num_episodes = 2000
     rList = []
 
-    env.render()
+    # env.render()
+    BATCH_SIZE = 64
+    TARGET_UPDATE_FREQUENCY = 5
+    REPLAY_MEMORY = 50000
 
-    init = tf.global_variables_initializer()
+    replay_buffer = deque(maxlen=REPLAY_MEMORY)
+
+    last_100_game_reward = deque(maxlen=100)
 
     with tf.Session() as sess:
-        sess.run( init )
+        mainDQN = DQN(sess, input_size, output_size, name="main")
+        targetDQN = DQN(sess, input_size, output_size, name="target")
+        sess.run( tf.global_variables_initializer() )
+
+        # initial copy q_net -> target_net
+        copy_ops = get_copy_var_ops(dest_scope_name="target",
+                                    src_scope_name="main")
+        sess.run(copy_ops)
+
+
         for i in range( num_episodes ):
             s = env.reset()
             e = 1.0 / ((i/50)+10)
             rAll = 0
             done = False
             local_loss = []
+            step_count = 0
 
             while not done:
-                Qs = sess.run( Qpred, feed_dict={X:one_hot(s, input_size)} )
+                # Qs = sess.run( Qpred, feed_dict={X:one_hot(s, input_size)} )
                 if np.random.rand(1) >= e:
-                    a = env.action_space.select( Qs[0] )
+                    a = env.action_space.select(mainDQN.predict( one_hot(s, input_size) )[0])
+                    # a = env.action_space.select( Qs[0] )
                 else:
                     a = env.action_space.sample()
 
-                print( a, env.action_space._player._deck )
+                # print( a, env.action_space._player._deck )
                 s1, reward, done, _ = env.step( a )
 
-                if done:
-                    Qs[0, a] = reward
-                else:
-                    Qs1 = sess.run( Qpred, feed_dict={ X:one_hot(s1, input_size)})
-                    Qs[0, a] = reward + discount*np.max( Qs1 )
+                # Save the experience to our buffer
+                replay_buffer.append((one_hot(s, input_size), a, reward, one_hot(s1, input_size), done))
 
-                sess.run( train, feed_dict={X: one_hot(s, input_size), Y:Qs} )
+                if len(replay_buffer) > BATCH_SIZE:
+                    minibatch = random.sample(replay_buffer, BATCH_SIZE)
+                    loss, _ = replay_train(mainDQN, targetDQN, minibatch)
 
+                if step_count % TARGET_UPDATE_FREQUENCY == 0:
+                    sess.run(copy_ops)
+
+                # if done:
+                #     Qs[0, a] = reward
+                #     if reward > 0:
+                #         print( env.action_space.getDumped(), env.Opponent.dumped )
+                # else:
+                #     Qs1 = sess.run( Qpred, feed_dict={ X:one_hot(s1, input_size)})
+                #     Qs[0, a] = reward + discount*np.max( Qs1 )
+                #
+                # sess.run( train, feed_dict={X: one_hot(s, input_size), Y:Qs} )
+                #
                 rAll += reward
                 s = s1
-            rList.append( rAll )
+                step_count += 1
 
-        print(W.eval(sess))
+            last_100_game_reward.append(step_count)
 
-    print( "P:"+str( sum(rList)/num_episodes))
+            if len(last_100_game_reward) == last_100_game_reward.maxlen:
+                avg_reward = np.mean(last_100_game_reward)
+
+                if avg_reward > 199:
+                    print(f"Game Cleared in {episode} episodes with avg reward {avg_reward}")
+                    break
+            # rList.append( rAll )
+
+        # print(W.eval(sess))
+
+    # print( "P:"+str( sum(rList)/num_episodes))
 
 
     plt.bar( range(len(rList)), rList, color="blue")
