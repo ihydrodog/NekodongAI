5241edca4b4821da765a661a3fe7642f7cc982ac
hydro <ihydrodog@gmail.com>
2018-03-30 08:30:10 +0900

    save progress

--- Nekodong.py
+++ Nekodong.py
@@ -7,127 +7,9 @@ import random
 import math
 import itertools
 from collections import deque
-from typing import List
 
 
-class DQN:
-
-    def __init__(self, session: tf.Session, input_size: int, output_size: int, name: str="main") -> None:
-        """DQN Agent can
-        1) Build network
-        2) Predict Q_value given state
-        3) Train parameters
-        Args:
-            session (tf.Session): Tensorflow session
-            input_size (int): Input dimension
-            output_size (int): Number of discrete actions
-            name (str, optional): TF Graph will be built under this name scope
-        """
-        self.session = session
-        self.input_size = input_size
-        self.output_size = output_size
-        self.net_name = name
-
-        self._build_network( h_size=input_size*2)
-
-    def _build_network(self, h_size=16, l_rate=0.001) -> None:
-        """DQN Network architecture (simple MLP)
-        Args:
-            h_size (int, optional): Hidden layer dimension
-            l_rate (float, optional): Learning rate
-        """
-        with tf.variable_scope(self.net_name):
-            self._X = tf.placeholder(tf.float32, [None, self.input_size], name="input_x")
-            net = self._X
-
-            net = tf.layers.dense(net, h_size, activation=tf.nn.relu)
-
-            net = tf.layers.dense(net, self.output_size)
-            self._Qpred = net
-
-            self._Y = tf.placeholder(tf.float32, shape=[None, self.output_size])
-            self._loss = tf.losses.mean_squared_error(self._Y, self._Qpred)
-
-            optimizer = tf.train.AdamOptimizer(learning_rate=l_rate)
-            self._train = optimizer.minimize(self._loss)
-
-    def predict(self, state: np.ndarray) -> np.ndarray:
-        """Returns Q(s, a)
-        Args:
-            state (np.ndarray): State array, shape (n, input_dim)
-        Returns:
-            np.ndarray: Q value array, shape (n, output_dim)
-        """
-        x = np.reshape(state, [-1, self.input_size])
-        return self.session.run(self._Qpred, feed_dict={self._X: x})
-
-    def update(self, x_stack: np.ndarray, y_stack: np.ndarray) -> list:
-        """Performs updates on given X and y and returns a result
-        Args:
-            x_stack (np.ndarray): State array, shape (n, input_dim)
-            y_stack (np.ndarray): Target Q array, shape (n, output_dim)
-        Returns:
-            list: First element is loss, second element is a result from train step
-        """
-        feed = {
-            self._X: x_stack,
-            self._Y: y_stack
-        }
-        return self.session.run([self._loss, self._train], feed)
-
-
-
-def replay_train(mainDQN: DQN, targetDQN: DQN, train_batch: list) -> float:
-    """Trains `mainDQN` with target Q values given by `targetDQN`
-    Args:
-        mainDQN (dqn.DQN): Main DQN that will be trained
-        targetDQN (dqn.DQN): Target DQN that will predict Q_target
-        train_batch (list): Minibatch of replay memory
-            Each element is (s, a, r, s', done)
-            [(state, action, reward, next_state, done), ...]
-    Returns:
-        float: After updating `mainDQN`, it returns a `loss`
-    """
-    DISCOUNT_RATE = 0.99
-
-
-    states = np.vstack([x[0] for x in train_batch])
-    actions = np.array([x[1] for x in train_batch])
-    rewards = np.array([x[2] for x in train_batch])
-    next_states = np.vstack([x[3] for x in train_batch])
-    done = np.array([x[4] for x in train_batch])
-
-    X = states
-
-    Q_target = rewards + DISCOUNT_RATE * np.max(targetDQN.predict(next_states), axis=1) * ~done
-
-    y = mainDQN.predict(states)
-    y[np.arange(len(X)), actions] = Q_target
-
-    # Train our network using target and predicted Q values on each episode
-    return mainDQN.update(X, y)
-
-
-def get_copy_var_ops(*, dest_scope_name: str, src_scope_name: str) -> List[tf.Operation]:
-    """Creates TF operations that copy weights from `src_scope` to `dest_scope`
-    Args:
-        dest_scope_name (str): Destination weights (copy to)
-        src_scope_name (str): Source weight (copy from)
-    Returns:
-        List[tf.Operation]: Update operations are created and returned
-    """
-    # Copy variables src_scope to dest_scope
-    op_holder = []
-
-    src_vars = tf.get_collection(
-        tf.GraphKeys.TRAINABLE_VARIABLES, scope=src_scope_name)
-    dest_vars = tf.get_collection(
-        tf.GraphKeys.TRAINABLE_VARIABLES, scope=dest_scope_name)
-
-    for src_var, dest_var in zip(src_vars, dest_vars):
-        op_holder.append(dest_var.assign(src_var.value()))
-
-    return op_holder
+from DQN import DQN
 
 
 
@@ -143,10 +25,14 @@ class CardPlay( gym.Env ):
             return random.choice( self._deck )
 
 
-        def strategy1(self):
-            return self._deck[0]
+        def strategy(self, t):
+            if t == 0:
+                return self._deck[0]
+            elif t == 1:
+                return self._deck[-1]
+            elif t == 2:
+                return self._deck[ len(self._deck)//2 ]
             # return random.choice( self._deck )
-
 
         def contains(self, x):
             # print( self._deck, x)
@@ -270,7 +156,8 @@ class CardPlay( gym.Env ):
             self.action_space.step( action )
             done = self.action_space.isDone()
 
-            opponentCard = self._opponent.strategy1()
+            opponentCard = self._opponent.strategy ( self.strategy)
+
             # opponentCard = self._opponent.randomChoice()
             self._opponent.pop( opponentCard )
 
@@ -319,6 +206,7 @@ class CardPlay( gym.Env ):
 
 
     def _reset(self):
+        self.strategy = random.randint( 0, 2 )
         self.action_space.reset()
         self._opponent.reset()
         self._state = self.action_space.getDumped() + self._opponent.dumped
@@ -347,7 +235,7 @@ def testRun():
     #
     # train = tf.train.GradientDescentOptimizer( learning_rate=0.1).minimize( loss )
     # discount = 0.99
-    num_episodes = 100000
+    num_episodes = 10000
     rList = []
 
     # env.render()
@@ -365,7 +253,7 @@ def testRun():
         sess.run( tf.global_variables_initializer() )
 
         # initial copy q_net -> target_net
-        copy_ops = get_copy_var_ops(dest_scope_name="target",
+        copy_ops = DQN.get_copy_var_ops(dest_scope_name="target",
                                     src_scope_name="main")
         sess.run(copy_ops)
 
@@ -396,7 +284,7 @@ def testRun():
 
                 if len(replay_buffer) > BATCH_SIZE:
                     minibatch = random.sample(replay_buffer, BATCH_SIZE)
-                    loss, _ = replay_train(mainDQN, targetDQN, minibatch)
+                    loss, _ = DQN.replay_train(mainDQN, targetDQN, minibatch)
 
                 if step_count % TARGET_UPDATE_FREQUENCY == 0:
                     sess.run(copy_ops)
